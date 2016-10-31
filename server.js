309475ff8ab4020903a522b9e112ca7710e35bb7
Fausto Brito <G30@Faustos-MacBook-Air.local>
2016-10-30 20:40:19 -0400

    path

--- server.js
+++ server.js
@@ -9,21 +9,14 @@ var app = express();
 //and with a live host////////
 var PORT = process.env.PORT || 4040;
  //Routes for local host
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json 
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require('./app/routing/html-routes.js')(app);
 
 //Added console log for confirmation purposes//
 app.listen(PORT, function(){
